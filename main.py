54b61f9180863ca6fabe14b24be5ac6b05d251d7
Maxli <maxli@maxlicatenby.org>
2021-01-11 14:08:17 -0500

    Can draw lines on the image

--- main.py
+++ main.py
@@ -17,12 +17,40 @@ SIZE = WIDTH, HEIGHT = (900, 600)
 
 screen = pygame.display.set_mode(SIZE, FLAGS)
 
+def fit_image(image):
+    sized_image = pygame.transform.smoothscale(image, SIZE)
+    screen.blit(sized_image, (0,0))
+
+def draw_dots(points):
+    if len(points) == 0: return
+    for i in points:
+        pygame.draw.circle(screen, blue, i, 3)
+
+def draw_lines(points, closed=False):
+    #print(f"{points=}")
+    if len(points) < 2: return
+    pygame.draw.lines(screen, blue, closed, points)
+
+def shift(image, points):
+    pass
+
+def scale_points(points, old_size, new_size):
+    pass
+
 
 if __name__ == '__main__':
     screen.fill(white)
     pygame.display.flip()
 
-    file_path = filedialog.askopenfilename()
+    file_path = filedialog.askopenfilename(filetypes=[("jpegs","*.jpg"), ("pngs","*.png"), ("All Files", "*.*")])
+    if file_path != "":
+        image = pygame.image.load(file_path)
+        fit_image(image)
+    else:
+        pygame.quit()
+        exit()
+
+    points = []
 
     while True:
         pygame.display.flip()
@@ -32,4 +60,16 @@ if __name__ == '__main__':
                 exit()
             if event.type == pygame.locals.VIDEORESIZE:
                 SIZE = WIDTH, HEIGHT = event.w, event.h
-                screen.fill(white)
+                points = []
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if len(points) >= 4:
+                    points = []
+                pos = pygame.mouse.get_pos()
+                points.append(pos)
+
+        fit_image(image)
+        draw_dots(points)
+        if len(points) < 4:
+            draw_lines(points + [pygame.mouse.get_pos()])
+        else:
+            draw_lines(points, True)
